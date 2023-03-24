0fffacf74444d51faea4a5f035b93b19c37a4bb0
Satoshi Watanabe <sassy.watanabe@gmail.com>
2023-03-24 20:28:01 +0900

    parseを別ファイルにする

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,7 @@ use std::{collections::HashMap, io::Write};
 
 mod tokenize;
 mod expr;
-
-// tokenを構文木に変換
-fn parse(tokens: &[String]) -> Result<(expr::Expr, &[String]), String> {
-    if tokens.is_empty() {
-        return Err("Unexpected end of input".to_string());
-    }
-
-    let token = tokens[0].clone();
-    let mut rest= &tokens[1..];
-
-    if token == "(" {
-        let mut list = Vec::new();
-        loop {
-            let (expr, new_rest) = parse(rest)?;
-            rest = new_rest;
-            if rest.is_empty() {
-                return  Err("Unexpected end of input".to_string());
-            }
-            list.push(expr);
-            if rest[0] == ")" {
-                rest = &rest[1..];
-                if list.len() == 3 && list[0] == expr::Expr::Symbol("cons".to_string()) {
-                    let car = list[1].clone();
-                    let cdr = list[2].clone();
-                    let cons = expr::Expr::Cons(Box::new((car, cdr)));
-                    return Ok((cons, rest));
-                }
-                return Ok((expr::Expr::List(list), rest));
-            }
-        }        
-    } else if token == ")" {
-        return Err("Unexpected ')".to_string());
-    } else {
-        if let Ok(int) = token.parse::<i32>() {
-            return Ok((expr::Expr::Int(int), rest));
-        } else {
-            return Ok((expr::Expr::Symbol(token), rest));
-        }
-    }
-}
+mod parse;
 
 // 構文木を再帰的に評価する
 fn eval(expr: &expr::Expr, env: &mut HashMap<String, i32>) -> Result<expr::Expr, String> {
@@ -274,7 +235,7 @@ fn eval(expr: &expr::Expr, env: &mut HashMap<String, i32>) -> Result<expr::Expr,
 
 fn eval_str(program: &str) -> Result<expr::Expr, String> {
     let tokens = tokenize::tokenize(program);
-    let (expr, rest) = parse(&tokens)?;
+    let (expr, rest) = parse::parse(&tokens)?;
     if !rest.is_empty() {
         return Err("Unexpected trailing tokens".to_string());
     }
@@ -305,25 +266,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse, eval_str, expr};
+    use crate::eval_str;
 
-    #[test]
-    fn test_parse() {
-        let tokens = vec![
-            "(".to_string(),
-            "+".to_string(),
-            "2".to_string(),
-            "3".to_string(),
-            ")".to_string()
-        ];
-        let (expr, rest) = parse(&tokens).unwrap();
-        assert_eq!(expr, expr::Expr::List(vec![
-            expr::Expr::Symbol("+".to_string()),
-            expr::Expr::Int(2),
-            expr::Expr::Int(3),
-        ]));
-        assert_eq!(rest, Vec::<String>::new());
-    }
+   
     #[test]
     fn test_eval_str() {
         let result = eval_str("(* 2 3)").unwrap().expr_str();
